1171e288d4ca37718abd3b698a135a8cd2bab7d7
PeterQ <PeterQ1996@gmail.com>
2017-08-13 13:21:06 +0800

    add http/request attr: method, change path attr

--- src/EzSwoole/Core/Protocol/Http/Request.php
+++ src/EzSwoole/Core/Protocol/Http/Request.php
@@ -16,9 +16,10 @@ class Request {
         // array('get' => $_GET, 'post' => $_POST,
         //    'cookie' => $_COOKIE, 'server' => $_SERVER, 'files' => $_FILES);
         $this->get = $arr['get'];
+        $this->method = $arr['server']['REQUEST_METHOD'];
         $this->post = $arr['post'];
         $this->cookie = $arr['cookie'];
         $this->server = $arr['server'];
-        $this->path = $arr['server']['REQUEST_URI'];
+        $this->path = explode('?',$arr['server']['REQUEST_URI'])[0];
     }
 }
